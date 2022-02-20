bd6d129e51329319a0a43b6dd34979419454b26f
thunderbioink <allealle0303@gmail.com>
2022-02-19 17:22:01 -0700

    web app

--- __main__.py
+++ __main__.py
@@ -1,1 +1,16 @@
 print('HELLO WORLD')
+# "Check if venv is active"
+# import sys
+
+# def get_base_prefix():
+#     """Get base/real prefix, or sys.prefix if there is none."""
+#     return getattr(sys, "base_prefix", None) or getattr(sys, "real_prefix", None) or sys.prefix
+
+# def in_virtualenv():
+#     if get_base_prefix() != sys.prefix:
+#         in_venv = print("in venv")
+#         return in_venv, "in venv"
+#     if get_base_prefix() == sys.prefix:
+#         not_in_venv = print("not in venv")
+#         return  not_in_venv, "not in venv"
+#     # assert ""
